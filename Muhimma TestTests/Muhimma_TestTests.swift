b2ecd1659cba7a11844b0fd19f6b5e89d2a72ad6
Moeen Ahmad <moeen@efatoora.ae>
2019-11-02 17:16:41 +0400

    Initial Commit

---
+++ Muhimma TestTests/Muhimma_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Muhimma_TestTests.swift
+//  Muhimma TestTests
+//
+//  Created by eFatoora LLC on 02/11/2019.
+//  Copyright © 2019 Muhimma.com.ae. All rights reserved.
+//
+
+import XCTest
+@testable import Muhimma_Test
+
+class Muhimma_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
